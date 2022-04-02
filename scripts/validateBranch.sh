b17d67ab94a792a7f8b9259a2c5a415da02de230
jonty007 <vivek.singh2994@gmail.com>
2022-04-02 15:51:52 +0530

    Setup push validations

--- scripts/validateBranch.sh
+++ scripts/validateBranch.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-var=$(git branch --show-current 2>&1)
-if ["$var" == "husky"]
-then
- echo "git push to husky branch not allowed"
- exit 1
+validate_branch() {
+    var="$(git branch --show-current 2>&1)"
+    
+    notAllowedBranch="development"
+
+    if [ $var == $notAllowedBranch ]
+    then
+        echo "git push to husky branch not allowed"
+        exit 1
+    fi
+}
