7a83666f872496fc9c8fa7978fc0b57fba9f5af6
jonty007 <vivek.singh2994@gmail.com>
2022-05-27 13:24:01 +0530

    release changes

--- config/config.js
+++ config/config.js
@@ -1,9 +1,7 @@
-const CHAIN = process.env.CHAIN || 'LTC'
-const NETWORK = process.env.NETWORK || 'testnet' // testnet or regtest
-const BCN_URL = process.env.BCN_URL || 'https://node.bitcoincomputer.io'
-
 export default {
-  CHAIN,
-  NETWORK,
-  BCN_URL,
+  CHAIN: process.env.CHAIN || 'LTC',
+  NETWORK: process.env.NETWORK || 'testnet',
+  BCN_URL: process.env.BCN_URL || 'https://node.bitcoincomputer.io',
+  TEST_SEEDS:
+    'travel upgrade inside soda birth essence junk merit never twenty system opinion;toddler hockey salute wheel harvest video narrow riot guitar lake sea call;cannon hour begin test replace fury motion squirrel envelope announce neck culture',
 }
--- scripts/validate-branch.sh
+++ scripts/validate-branch.sh
@@ -1,12 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 
 validate_branch() {
-    echo 'yo'
-#     var="$(git branch --show-current 2>&1)"
+    var="$(git branch --show-current 2>&1)"
 
-#     if [ "$var" = "dev" ] || [ "$var" = "master" ]
-#     then
-#         echo "git push to dev or master branch not allowed. Please create PR"
-#         exit 1
-#     fi
+    if [ "$var" = "dev" ] || [ "$var" = "master" ]
+    then
+        echo "git push to dev or master branch not allowed. Please create PR"
+        exit 1
+    fi
 }
